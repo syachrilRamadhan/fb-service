73bc95eb06f4246bad1f8b469c637ec488c9962a
syachrilRamadhan <syachrilbls99@gmail.com>
2024-08-13 23:27:38 +0700

    feature: (auth) add jwt token

--- controller/authcontroller/authController.go
+++ controller/authcontroller/authController.go
@@ -2,15 +2,81 @@ package authcontroller
 
 import (
 	"encoding/json"
+	"fb-service/config"
 	"fb-service/helper"
 	"fb-service/models"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
 
+	var inputUser models.User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&inputUser); err != nil {
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJson(w, http.StatusBadRequest, response)
+		return
+	}
+	defer r.Body.Close()
+
+	//get data user
+	var user models.User
+
+	if err := models.DB.Select("username, password").Where("username = ?", inputUser.Username).First(&user).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			response := map[string]string{"message": "Username not found"}
+			helper.ResponseJson(w, http.StatusNotFound, response)
+			return
+		default:
+			response := map[string]string{"message": err.Error()}
+			helper.ResponseJson(w, http.StatusInternalServerError, response)
+			return
+		}
+	}
+
+	//matching passwords match
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(inputUser.Password)); err != nil {
+		response := map[string]string{"message": "Username atau password salah!"}
+		helper.ResponseJson(w, http.StatusUnauthorized, response)
+		return
+	}
+
+	//create jwt token
+	expTime := time.Now().Add(time.Minute * 60)
+	claims := &config.JWTClaim{
+		Username: user.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "fb-service",
+			ExpiresAt: jwt.NewNumericDate(expTime),
+		},
+	}
+
+	//declare signing algorithm
+	tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	//signed token
+	token, err := tokenAlgo.SignedString(config.JWT_KEY)
+	if err != nil {
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJson(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	//set token into cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Path:     "/",
+		Value:    token,
+		HttpOnly: true,
+	})
+
+	response := map[string]string{"message": "Login Success"}
+	helper.ResponseJson(w, http.StatusOK, response)
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +87,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	if err := decoder.Decode(&inputUser); err != nil {
 		response := map[string]string{"message": err.Error()}
 		helper.ResponseJson(w, http.StatusBadRequest, response)
+		return
 	}
 	defer r.Body.Close()
 
@@ -32,6 +99,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	if err := models.DB.Create(&inputUser).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		helper.ResponseJson(w, http.StatusInternalServerError, response)
+		return
 	}
 
 	response := map[string]string{"message": "Create data successfully"}
--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	Id          int64  `gorm:"primaryKey" json:"id"`
-	NamaLengkap string `gorm:"varchar(100)" json:"nama_lengkap"`
-	Username    string `gorm:"varchar(100)" json:"username"`
-	Password    string `gorm:"varchar(100)" json:"password"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Id          int64     `gorm:"primaryKey" json:"id"`
+	NamaLengkap string    `gorm:"varchar(100)" json:"nama_lengkap"`
+	Username    string    `gorm:"varchar(100),unique" json:"username"`
+	Password    string    `gorm:"varchar(100)" json:"password"`
+	CreatedAt   time.Time `gorm:"column:created_at"`
+	UpdatedAt   time.Time `gorm:"column:updated_at"`
 }
